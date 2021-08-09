942dad140c27d82aa3ee5ef5f9a13f9f66da4b2a
anushabc <66515706+anushabc@users.noreply.github.com>
2021-08-09 23:31:31 +0530

    Create extension.bat

---
+++ extension.bat
@@ -0,0 +1,2 @@
+code --install-extension ms-dotnettools.csharp code --install-extension vsciot-vscode.azure-iot-tools code --install-extension ms-azuretools.vscode-azurefunctions
+az extension add --name azure-iot
