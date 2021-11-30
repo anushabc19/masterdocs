128ce43d52fbb7f5353746d485dc372b402689f2
anushabc <66515706+anushabc@users.noreply.github.com>
2021-11-30 18:54:21 +0530

    Update extension.bat

--- extension.bat
+++ extension.bat
@@ -1,2 +1,2 @@
-code --install-extension ms-dotnettools.csharp code --install-extension vsciot-vscode.azure-iot-tools code --install-extension ms-azuretools.vscode-azurefunctions
-az extension add --name azure-iot
+code --install-extension ms-dotnettools.csharp code --install-extension vsciot-vscode.azure-iot-tools code --install-extension ms-azuretools.vscode-azurefunctions code --install-extension ms-azuretools.vscode-docker
+az extension add --name azure-iot 
