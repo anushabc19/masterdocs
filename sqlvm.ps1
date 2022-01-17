0730e6d6c136a65896f7983acc0d7ed6c0c00d9b
anushabc <66515706+anushabc@users.noreply.github.com>
2022-01-17 19:27:56 +0530

    Update sqlvm.ps1

--- sqlvm.ps1
+++ sqlvm.ps1
@@ -1,77 +1,30 @@
-Param (
-    [Parameter(Mandatory = $true)]
-    [string]
-    $AzureUserName,
-
-    [string]
-    $AzurePassword,
-
-    [string]
-    $AzureTenantID,
-
-    [string]
-    $AzureSubscriptionID,
-
-    [string]
-    $ODLID,
-
-    [string]
-    $DeploymentID,
-
-    [string]
-    $InstallCloudLabsShadow,
-
-    [string]
-    $SPDisplayName,
-
-    [string]
-    $SPID,
-
-    [string]
-    $SPObjectID,
-
-    [string]
-    $SPSecretKey,
-
-    [string]
-    $AzureTenantDomainName,
-
-    [Parameter(Mandatory=$False)] [string] $SqlPass = ""
-
-)
-
-Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
-[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
-[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
-
-# Enable SQL Server ports on the Windows firewall
-function Add-SqlFirewallRule {
-    $fwPolicy = $null
-    $fwPolicy = New-Object -ComObject HNetCfg.FWPolicy2
-
-    $NewRule = $null
-    $NewRule = New-Object -ComObject HNetCfg.FWRule
-
-    $NewRule.Name = "SqlServer"
-    # TCP
-    $NewRule.Protocol = 6
-    $NewRule.LocalPorts = 1433
-    $NewRule.Enabled = $True
-    $NewRule.Grouping = "SQL Server"
-    # ALL
-    $NewRule.Profiles = 7
-    # ALLOW
-    $NewRule.Action = 1
-    # Add the new rule
-    $fwPolicy.Rules.Add($NewRule)
+# Disable Internet Explorer Enhanced Security Configuration
+function Disable-InternetExplorerESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force
+    Stop-Process -Name Explorer -Force
+    Write-Host 'IE Enhanced Security Configuration (ESC) has been disabled.' -ForegroundColor Green
 }
 
-Add-SqlFirewallRule
-$SqlPass="Password.1!!"
+# Force TLS 1.2 use instead of TLS 1.0
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Disable IE ESC
+Disable-InternetExplorerESC
 
 # Download the database backup file from the GitHub repo
 Invoke-WebRequest 'https://raw.githubusercontent.com/microsoft/MCW-Migrating-SQL-databases-to-Azure/master/Hands-on%20lab/lab-files/Database/WideWorldImporters.bak' -OutFile 'C:\WideWorldImporters.bak'
 
+# Download and install Data Mirgation Assistant
+# Invoke-WebRequest 'https://download.microsoft.com/download/C/6/3/C63D8695-CEF2-43C3-AF0A-4989507E429B/DataMigrationAssistant.msi' -OutFile 'C:\DataMigrationAssistant.msi'
+# Start-Process -file 'C:\DataMigrationAssistant.msi' -arg '/qn /l*v C:\dma_install.txt' -passthru | wait-process
+
+# Wait a few minutes to allow the SQL Resource provider setup to start
+Start-Sleep -Seconds 240.0
+
+# Add snapins to allow use of the Invoke-SqlCmd commandlet
 Add-PSSnapin SqlServerProviderSnapin100 -ErrorAction SilentlyContinue
 Add-PSSnapin SqlServerCmdletSnapin100 -ErrorAction SilentlyContinue
 
@@ -98,64 +51,56 @@ function Restore-SqlDatabase {
     Start-Sleep -Seconds 30
 }
 
-# Download and install Data Mirgation Assistant
-(New-Object System.Net.WebClient).DownloadFile('https://download.microsoft.com/download/C/6/3/C63D8695-CEF2-43C3-AF0A-4989507E429B/DataMigrationAssistant.msi', 'C:\DataMigrationAssistant.msi')
-Start-Process -file 'C:\DataMigrationAssistant.msi' -arg '/qn /l*v C:\dma_install.txt' -passthru | wait-process
+function Enable-ServiceBroker {
+    $SetBrokerCmd = "USE [$DatabaseName];
+                     GO
+                     GRANT ALTER ON DATABASE:: $DatabaseName TO $SqlMiUser;
+                     GO
+                     ALTER DATABASE [$DatabaseName]
+                     SET ENABLE_BROKER WITH ROLLBACK IMMEDIATE;
+                     GO"
 
-# Attach the downloaded backup files to the local SQL Server instance
-function Setup-Sql {
-    #Add snap-in
-    Add-PSSnapin SqlServerCmdletSnapin* -ErrorAction SilentlyContinue
-
-    $ServerName = "SQL2008-$DeploymentID"
-    $DatabaseName = 'PartsUnlimited'
-    
-    $Cmd = "USE [master] CREATE DATABASE [$DatabaseName]"
-    Invoke-Sqlcmd $Cmd -QueryTimeout 3600 -ServerInstance $ServerName
-
-    Invoke-Sqlcmd "ALTER DATABASE [$DatabaseName] SET DISABLE_BROKER;" -QueryTimeout 3600 -ServerInstance $ServerName
-    
-    Invoke-Sqlcmd "CREATE LOGIN PUWebSite WITH PASSWORD = '$SqlPass';" -QueryTimeout 3600 -ServerInstance $ServerName
-    Invoke-Sqlcmd "USE [$DatabaseName];CREATE USER PUWebSite FOR LOGIN [PUWebSite];EXEC sp_addrolemember 'db_owner', 'PUWebSite'; " -QueryTimeout 3600 -ServerInstance $ServerName
-
-    Invoke-Sqlcmd "EXEC sp_addsrvrolemember @loginame = N'PUWebSite', @rolename = N'sysadmin';" -QueryTimeout 3600 -ServerInstance $ServerName
-
-    Invoke-Sqlcmd "EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', N'Software\Microsoft\MSSQLServer\MSSQLServer', N'LoginMode', REG_DWORD, 2" -QueryTimeout 3600 -ServerInstance $ServerName
-
-    Restart-Service -Force MSSQLSERVER
-    #In case restart failed but service was shut down.
-    Start-Service -Name 'MSSQLSERVER' 
+    Invoke-SqlCmd -Query $SetBrokerCmd -QueryTimeout 3600 -Username $SqlMiUser -Password $Password -ServerInstance $ServerName
 }
-Setup-Sql
 
-$hostname= "$env:ComputerName"
-$path="C:\CloudLabs\Validator\installer.bat"
-(Get-Content -Path "$path") | ForEach-Object {$_ -Replace "serverName", "$hostname"} | Set-Content -Path "$path"
+function Config-SqlDatabaseLogin {
+    $UserName = 'WorkshopUser'
 
-Function InstallCloudLabsShadow($odlid, $InstallCloudLabsShadow)
-    {
-        if($InstallCloudLabsShadow -eq 'yes')
-        {
-            $WebClient = New-Object System.Net.WebClient
-            $url1 = "https://spektrasystems.screenconnect.com/Bin/ConnectWiseControl.ClientSetup.msi?h=instance-ma1weu-relay.screenconnect.com&p=443&k=BgIAAACkAABSU0ExAAgAAAEAAQDhrCYwK%2BhPzyOyTYW71BahP4Q7hsWvkU20udO6d7cGuH8VAADzVNnsk39zavkgVu2uLHR1mfAL%2BUd6iAJOofhlcjO%2FB%2FVAEwvqtQ7403Nqm6rGvy6%2FxHEiqvzvn42JADRxdGVFaw9SYyTi4QckGjG0OnG69mW2RBQdWOZ3FKmhJD6zWRPZVTbl7gJkpIdMZx0BbWKiYVsvJYgoCWNXIqqH77rigu5dsmEnWeC9J0Or1KaU%2Bzsd6QJwAzEwomhiGp3FII4wbGBnCiHLD%2FrtNgR%2BJ1H3bKgYkesdxuFvO5DzUc3eEOVBSwR0crd06J%2BJP4DolgWWNZN6ZmQ1s5aOQgSq&e=Access&y=Guest&t=&c="
-            $url3 = "&c=&c=&c=&c=&c=&c=&c="
-            $finalurl = $url1 + $odlid + $url3
-            $WebClient.DownloadFile("$finalurl","C:\Packages\cloudlabsshadow.msi")
-            Start-Process msiexec.exe -Wait '/I C:\Packages\cloudlabsshadow.msi /qn' -Verbose
-        }
-    }
+    $CreateLoginCmd = "USE [master];
+                       GO
+                       CREATE LOGIN $UserName WITH PASSWORD = N'$Password';
+                       GO"
 
-    InstallCloudLabsShadow $ODLID $InstallCloudLabsShadow
-#Import Common Functions
-$path = pwd
-$path=$path.Path
-$commonscriptpath = "$path" + "\cloudlabs-common\cloudlabs-windows-functions.ps1"
-. $commonscriptpath
+    Invoke-SqlCmd -Query $CreateLoginCmd -QueryTimeout 3600 -Username $SqlMiUser -Password $Password -ServerInstance $ServerName
 
-# Run Imported functions from cloudlabs-windows-functions.ps1
-WindowsServerCommon
-#InstallCloudLabsShadow $ODLID $InstallCloudLabsShadow
-CreateCredFile $AzureUserName $AzurePassword $AzureTenantID $AzureSubscriptionID $DeploymentID
-SPtoAzureCredFiles $SPDisplayName $SPID $SPObjectID $SPSecretKey $AzureTenantDomainName
+    $AddRoleCmd = "USE [master];
+                   GO
+                   EXEC sp_addsrvrolemember @loginame = N'$UserName', @rolename = N'sysadmin';
+                   GO"
 
-InstallLegacyVmValidator
+    Invoke-SqlCmd -Query $AddRoleCmd -QueryTimeout 3600 -Username $SqlMiUser -Password $Password -ServerInstance $ServerName
+
+    $AssignUserCmd = "USE [$DatabaseName];
+                      GO
+                      IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = '$UserName')
+                        BEGIN
+                            CREATE USER [$UserName] FOR LOGIN [$UserName]
+                            EXEC sp_addrolemember N'db_datareader', N'$UserName'
+                        END;
+                      GO"
+
+    Invoke-SqlCmd -Query $AssignUserCmd -QueryTimeout 3600 -Username $SqlMiUser -Password $Password -ServerInstance $ServerName
+}
+
+# Restore the datasbase
+Restore-SqlDatabase
+
+# Restart the MSSQLSERVER service.
+Stop-Service -Name 'MSSQLSERVER' -Force
+Start-Service -Name 'MSSQLSERVER'
+
+# Enable the Service Broker functionality on the database
+Enable-ServiceBroker
+
+# Create the WorkshopUser user
+Config-SqlDatabaseLogin
