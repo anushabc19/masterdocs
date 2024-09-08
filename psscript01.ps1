2dc4b7afbcd3c0b494bc397978255ebb74615055
anushabc19 <166631447+anushabc19@users.noreply.github.com>
2024-09-08 19:34:54 +0530

    Update psscript01.ps1

--- psscript01.ps1
+++ psscript01.ps1
@@ -1,99 +1,34 @@
 Param (
-    [Parameter(Mandatory = $true)]
-    [string]
-    $AzureUserName,
-    [string]
-    $AzurePassword,
-    [string]
-    $AzureTenantID,
-    [string]
-    $AzureSubscriptionID,
-    [string]
-    $ODLID,
-    [string]
-    $DeploymentID,
-    [string]
-    $azuserobjectid,
-    [string]
-    $InstallCloudLabsShadow,
-     
-    [string]
-    $vmAdminUsername,
-
-    [string]
-    $trainerUserName,
-
-    [string]
-    $trainerUserPassword
-
 )
 
-Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
-[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
-[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
-$adminUsername = "demouser"
+function Install-Chrome {
+    param (
+        [string]$DownloadUrl = "https://dl.google.com/chrome/install/latest/chrome_installer.exe",
+        [string]$InstallerPath = "$env:TEMP\chrome_installer.exe"
+    )
 
-#Import Common Functions
-$path = pwd
-$path=$path.Path
-$commonscriptpath = "$path" + "\cloudlabs-common\cloudlabs-windows-functions.ps1"
-. $commonscriptpath
+    # Download the Chrome installer
+    Write-Host "Downloading Google Chrome installer..."
+    Invoke-WebRequest -Uri $DownloadUrl -OutFile $InstallerPath
 
-# Run Imported functions from cloudlabs-windows-functions.ps1
-WindowsServerCommon
-InstallCloudLabsShadow $ODLID $InstallCloudLabsShadow
-CreateCredFile $AzureUserName $AzurePassword $AzureTenantID $AzureSubscriptionID $DeploymentID $azuserobjectid
-InstallVSCode
-choco install dotnetcore-sdk
-choco install azure-functions-core-tools
-InstallAzCLI
+    # Check if the installer was downloaded successfully
+    if (Test-Path $InstallerPath) {
+        Write-Host "Installer downloaded successfully."
 
-sleep 10
+        # Run the installer
+        Write-Host "Installing Google Chrome..."
+        Start-Process -FilePath $InstallerPath -Args "/silent /install" -Wait
 
-#Install synapse modules
-Install-PackageProvider NuGet -Force
+        # Clean up the installer file
+        Remove-Item -Path $InstallerPath
 
-#installing extensions to vscode
-code --install-extension ms-dotnettools.csharp 
-code --install-extension vsciot-vscode.azure-iot-tools
-code --install-extension ms-azuretools.vscode-azurefunctions
+        Write-Host "Google Chrome has been installed successfully."
+    } else {
+        Write-Host "Failed to download the installer."
+    }
+}
 
-sleep  10
+# Call the function to install Chrome
+Install-Chrome
 
-#Assign Packages to Install
-choco install vscode
-choco install git
-choco install nodejs.install
-
-sleep 5
-
-#DownloadFiles
-$WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://experienceazure.blob.core.windows.net/templates/innovate-and-modernize-apps-with-data-and-ai/scripts/extensions.bat","C:\Packages\extensions.bat")
-
-$WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://raw.githubusercontent.com/anushabc/masterdocs/main/logontask.ps1","C:\Packages\logontask.ps1")
-
-
-
-sleep 5
-
-Enable-CloudLabsEmbeddedShadow $vmAdminUsername $trainerUserName $trainerUserPassword
-
-
-#Enable Autologon
-$AutoLogonRegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
-Set-ItemProperty -Path $AutoLogonRegPath -Name "AutoAdminLogon" -Value "1" -type String 
-Set-ItemProperty -Path $AutoLogonRegPath -Name "DefaultUsername" -Value "$($env:ComputerName)\demouser" -type String  
-Set-ItemProperty -Path $AutoLogonRegPath -Name "DefaultPassword" -Value "Password.1!!" -type String
-Set-ItemProperty -Path $AutoLogonRegPath -Name "AutoLogonCount" -Value "1" -type DWord
-
-
-# Scheduled Task to Run PostConfig.ps1 screen on logon
-$Trigger= New-ScheduledTaskTrigger -AtLogOn
-$User= "$($env:ComputerName)\$adminUsername" 
-$Action= New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe" -Argument "-executionPolicy Unrestricted -File C:\Packages\logontask.ps1"
-Register-ScheduledTask -TaskName "SetUpVMs" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest -Force
-
-Stop-Transcript
 Restart-Computer -Force 
